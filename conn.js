9c677e923ef9105af0f69715c015bf1cfe50e9aa
Jonathan Ray <jonathanray17@gmail.com>
2019-03-25 14:34:33 -0400

    adds shortcuts to package.json

--- conn.js
+++ conn.js
@@ -1,12 +1,13 @@
 //First, require 'pg-promise'
 //Call it immediately, which gives us a configured database connector
 const pgp = require('pg-promise')();
-// next, define 
+// next, define the options for the connection to the database
 const options = {
     host: 'localhost',
     database: 'restaurants-app'
 };
 
+//make a connection to the database specified by the options object
 const db = pgp(options);
 
 db.any('select * from users usr where usr.id = 1;', [true])
